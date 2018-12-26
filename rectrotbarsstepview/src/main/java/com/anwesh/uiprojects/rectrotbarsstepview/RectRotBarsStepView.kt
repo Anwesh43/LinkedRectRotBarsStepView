610fddd1a3a9fafc5eabd6570603ed54ce5ef0a9
Anwesh43 <anweshthecool0@gmail.com>
2018-12-26 09:24:46 +0530

    adding constants and importing libraries

--- rectrotbarsstepview/src/main/java/com/anwesh/uiprojects/rectrotbarsstepview/RectRotBarsStepView.kt
+++ rectrotbarsstepview/src/main/java/com/anwesh/uiprojects/rectrotbarsstepview/RectRotBarsStepView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.rectrotbarsstepview
 /**
  * Created by anweshmishra on 26/12/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val rects : Int = 4
+val scDiv : Double = 0.51
+val scGap: Float = 0.05f
+val sizeFactor : Float = 2.6f
+val strokeFactor : Int = 90
+val color : Int = Color.parseColor("#0D47A1")
