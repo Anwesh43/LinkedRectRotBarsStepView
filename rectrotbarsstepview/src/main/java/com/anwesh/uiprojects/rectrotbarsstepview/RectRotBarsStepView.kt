949435f56a161b8eb0dedbf5fb8839efc05a08d7
Anwesh43 <anweshthecool0@gmail.com>
2018-12-26 10:39:26 +0530

    adding drawRRRBNode canvas extension method

--- rectrotbarsstepview/src/main/java/com/anwesh/uiprojects/rectrotbarsstepview/RectRotBarsStepView.kt
+++ rectrotbarsstepview/src/main/java/com/anwesh/uiprojects/rectrotbarsstepview/RectRotBarsStepView.kt
@@ -31,4 +31,34 @@ fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 
-fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap 
+fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawRRBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val xGap : Float = size / (rects + 1)
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = color 
+    val barSize : Float = size / 5
+    save()
+    translate(gap * (i + 1), h/2)
+    rotate(90f * sc2)
+    drawRect(RectF(-size, -size/2, size, size/2), paint)
+    for (k in 0..1) {
+        val sck : Float = sc1.divideScale(k, 2)
+        save()
+        scale(1f, 1f - 2 * k)
+        for (j in 0..(rects - 1)) {
+            val scj : Float = sck.divideScale(j, rects)
+            save()
+            translate(xGap, -size/2)
+            drawRect(RectF(-barSize/2, -barSize * scj, barSize/2, 0f), paint)
+            restore()
+        }
+        restore()
+    }
+    restore()
+}
