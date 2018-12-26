4df7e557d3be6eaee853c43aad224fad0e9f7822
Anwesh43 <anweshthecool0@gmail.com>
2018-12-26 11:26:13 +0530

    adding RRBNode class

--- rectrotbarsstepview/src/main/java/com/anwesh/uiprojects/rectrotbarsstepview/RectRotBarsStepView.kt
+++ rectrotbarsstepview/src/main/java/com/anwesh/uiprojects/rectrotbarsstepview/RectRotBarsStepView.kt
@@ -127,4 +127,47 @@ class RectRotBarsStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RRBNode(var i : Int, val state : State = State()) {
+
+        private var next : RRBNode? = null
+        private var prev : RRBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = RRBNode(i + 1)
+                prev = next
+            }
+        }
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRRBNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RRBNode {
+            var curr : RRBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
