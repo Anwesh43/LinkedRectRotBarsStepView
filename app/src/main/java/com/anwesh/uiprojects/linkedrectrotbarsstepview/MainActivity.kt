6619cdf6d4313ce186f98f6a19affa5a29bd24d2
Anwesh43 <anweshthecool0@gmail.com>
2018-12-26 12:10:14 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedrectrotbarsstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrectrotbarsstepview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedrectrotbarsstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.rectrotbarsstepview.RectRotBarsStepView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         RectRotBarsStepView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
