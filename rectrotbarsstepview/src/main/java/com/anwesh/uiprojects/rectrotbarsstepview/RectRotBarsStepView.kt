0ed891987c61f2fe238ff5f06f46231eeb482f58
Anwesh43 <anweshthecool0@gmail.com>
2018-12-26 10:20:45 +0530

    adding Int and Float extension methods

--- rectrotbarsstepview/src/main/java/com/anwesh/uiprojects/rectrotbarsstepview/RectRotBarsStepView.kt
+++ rectrotbarsstepview/src/main/java/com/anwesh/uiprojects/rectrotbarsstepview/RectRotBarsStepView.kt
@@ -20,3 +20,15 @@ val scGap: Float = 0.05f
 val sizeFactor : Float = 2.6f
 val strokeFactor : Int = 90
 val color : Int = Color.parseColor("#0D47A1")
+
+fun Int.inverse() : Float = 1f / this
+
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+
+fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
+
+fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap 
